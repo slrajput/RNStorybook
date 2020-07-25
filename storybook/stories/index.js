ef3b2b37db002973adfbcb8eaac8736c6f4f1d88
Manish Singh Rajput <mrajput@convene-tech.com>
2020-07-25 11:16:18 +0530

    add storybook

---
+++ storybook/stories/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { storiesOf } from '@storybook/react-native';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { WithFigma } from 'storybook-addon-figma';
+
+import Button from './Button';
+import CenterView from './CenterView';
+import Welcome from './Welcome';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+  .add('with text', () => (
+    <Button onPress={action('clicked-text')}>
+      <Text>Hello Button</Text>
+    </Button>
+  ))
+  .add('With Figma', () => (
+    <WithFigma
+      url={'https://www.figma.com/file/LbcvMJxDtshDmYtdyfJfkA72/Button-Primary'}
+    >
+      <Button onPress={action('clicked-button')}>
+      <Text>Figma button</Text>
+    </Button>
+    </WithFigma>
+  ))
+  .add('with some emoji', () => (
+    <Button onPress={action('clicked-emoji')}>
+      <Text>😀 😎 👍 💯</Text>
+    </Button>
+  ));
